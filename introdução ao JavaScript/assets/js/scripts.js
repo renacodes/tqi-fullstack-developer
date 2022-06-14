08d56b64199e33693b20bdbf5f35a8f456544a02
Angelo Rena <85187052+renacodes@users.noreply.github.com>
2022-06-14 20:49:20 -0300

    adicionadas condições do desafio
    - condições de parada do contador
    - mudanças de estilo
    - funções separadas

--- introdução ao JavaScript/assets/js/scripts.js
+++ introdução ao JavaScript/assets/js/scripts.js
@@ -3,11 +3,46 @@ var currentNumber = 0;
 const adicionar = document.getElementById("adicionar");
 const subtrair = document.getElementById("subtrair");
 var mensagem = document.getElementById("mensagem");
+
+function linkStyle() {
+  mensagem.style.cursor = "pointer";
+  mensagem.style.textDecoration = "underline";
+  mensagem.setAttribute("onMouseLeave", "styleOff()");
+}
+
+function styleOff() {
+  mensagem.style.cursor = "none";
+  mensagem.style.textDecoration = "none";
+}
+
+function reset() {
+  currentNumber = 0;
+  currentNumberWrapper.innerHTML = currentNumber;
+  mensagem.innerHTML =
+    "<em>Clique nos botões para aumentar ou diminuir o número mostrado.</em>";
+  mensagem.style.cursor = "none";
+  mensagem.style.textDecoration = "none";
+}
+
 adicionar.addEventListener("click", function () {
-  currentNumber++;
-  currentNumberWrapper.innerHTML = currentNumber;
+  if (currentNumber >= 100) {
+    mensagem.innerHTML = "<em>Ufa! Cansei... não quer começar de novo?</em>";
+    mensagem.setAttribute("onClick", "reset()");
+    mensagem.setAttribute("onMouseEnter", "linkStyle()");
+  } else {
+    currentNumber++;
+    currentNumberWrapper.innerHTML = currentNumber;
+  }
 });
+
 subtrair.addEventListener("click", function () {
-  currentNumber--;
-  currentNumberWrapper.innerHTML = currentNumber;
+  if (currentNumber == -10) {
+    mensagem.innerHTML =
+      "<em>Pra quê você precisa contar até -10?</em>";
+    mensagem.setAttribute("onClick", "reset()");
+    mensagem.setAttribute("onMouseEnter", "linkStyle()");
+  } else {
+    currentNumber--;
+    currentNumberWrapper.innerHTML = currentNumber;
+  }
 });
